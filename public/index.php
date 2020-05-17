83711a374876db5cb6fe1759909740cfa0e77fb5
rohitsraj12 <rohitsraj12@gmail.com>
2020-05-17 23:40:46 +0530

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+      $page_title = "home page";
+      include("../private/required/public_header.php");
+?>
+            <div class="body__container">
+                  <div class="home__page">
+                        <main>
+                        <?php
+                              echo $page_title;
+                        ?>
+                        </main>
+                  </div>
+            </div>
+            <div class="body__footer"></div>
+      </div>
+      <!-- script -->
+      <script src="js/jQuery.v3.4.1.js"></script>
+      <script src="js/baseScript.js"></script>
+</body>
+</html>
