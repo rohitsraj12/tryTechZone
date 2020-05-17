5bb2aca64b26deab02037743d5e13fbb49ac09e6
rohitsraj12 <rohitsraj12@gmail.com>
2020-05-17 23:41:24 +0530

    Create public_header.php

---
+++ private/required/public_header.php
@@ -0,0 +1,52 @@
+<?php 
+?>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title><?php echo $page_title; ?> | Try Tech Zone</title>
+
+      <!-- style sheet -->
+      <link rel="stylesheet" href="css/style.css">
+
+      <!-- script -->
+
+</head>
+<body>
+      <div class="body__wrap">
+            <div class="body__header">
+                  <div id="hamberger" class="hamberger">
+                        <i class="fa fa-twitter"></i>
+                  </div>
+                  <header>
+                        <div class="header__brand">
+                              <a href="index.php">TryTechZone</a>
+                        </div>
+                        <!-- end header brand -->
+
+                        <div class="header__link">
+                        
+                              <nav class="header__nav">
+                                    <ul>
+                                          <li class="nav__list"><a href="<?php //base_url(); ?>" class="nav__link">home</a></li>
+                                          <li class="nav__list"><a href="review.php" class="nav__link">reviews</a></li>
+                                          <li class="nav__list"><a href="about.php" class="nav__link">about</a></li>
+                                          <li class="nav__list"><a href="contact.php" class="nav__link">contact</a></li>
+                                    </ul>
+                              </nav>
+                              <!-- end header nav -->
+
+                              <div class="header__social">
+                                    <ul>
+                                          <li class="social__list"><a href="#" target="_blank" class="social__link"><i title="twitter" class="fab fa-twitter"></i>1</a></li>
+                                          <li class="social__list"><a href="#" target="_blank" class="social__link"><i title="facebook" class="fab fa-facebook-f"></i>2</a></li>
+                                          <li class="social__list"><a href="#" target="_blank" class="social__link"><i title="instagram" class="fab fa-instagram"></i>3</a></li>
+                                    </ul>
+                              </div>
+                              <!-- end header social -->
+                        </div>
+                        <!-- end header link -->
+                  </header>
+            </div>
+            <!-- end body header -->
