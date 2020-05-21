3d689baf58783d72ddb034fe55c0b84b2a5d8114
rohitsraj12 <rohitsraj12@gmail.com>
2020-05-21 20:41:45 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,10 +5,39 @@
             <div class="body__container">
                   <div class="home__page">
                         <main>
-                        <?php
-                              echo $page_title;
-                        ?>
+                              <section class="section__article">
+                                    <article class="section__article-block">
+                                          <figure class="article__image">
+                                                <img src="img/blog_img/blog-1.jpg" alt="">
+                                          </figure>
+                                          <header class="article__header">
+                                                <h1 class="heading-primary">
+                                                      Lorem ipsum dolor sit amet consectetur.
+                                                </h1>
+                                                <div class="class">1</div>
+    <div class="class">2</div>
+    <div class="class">3</div>
+    <div class="class">4</div>
+    <div class="class">5</div>
+    <div class="class">6</div>
+    <div class="class">7</div>
+    <div class="class">8</div>
+    <div class="class">9</div>
+    <div class="class">10</div>
+
+                                          </header>
+                                    </article>
+                                    <article class="section__article-block">
+
+                                    </article>
+                                    <article class="section__article-block">
+
+                                    </article>
+                              </section>
                         </main>
+                        <aside>
+
+                        </aside>
                   </div>
             </div>
             <div class="body__footer"></div>
